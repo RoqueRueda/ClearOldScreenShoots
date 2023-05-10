fad13264cf3f6fa2e63beace444d33d0ddca2a69
Roque Rueda <roque@whisper.sh>
2023-05-10 00:36:56 -0600

    Add license notice

---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class MainKtTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun main() {
+        // Nothing here to test yet
+    }
+}
